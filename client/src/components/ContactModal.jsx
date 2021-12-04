77265802ae9f47562a8a33c140bf533b844ef0ea
Jacob Peterson <petersonjacobw@gmail.com>
2021-12-04 14:37:49 -0800

    Completed form logic

--- client/src/components/ContactModal.jsx
+++ client/src/components/ContactModal.jsx
@@ -1,31 +1,60 @@
-import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import { StyledButton } from '../styles.js';
 
-const ContactModal = ({ show, onHide }) => (
-  <Modal
-    show={show}
-    onHide={onHide}
-    size="lg"
-    aria-labelledby="contained-modal-title-vcenter"
-    centered
-  >
-    <Modal.Header closeButton>
-      <Modal.Title id="contained-modal-title-vcenter">
-        Contact
-      </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <h4>Centered Modal</h4>
-      <p>
-        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-        consectetur ac, vestibulum at eros.
-      </p>
-    </Modal.Body>
-    <Modal.Footer>
-      <Button onClick={onHide}>Close</Button>
-    </Modal.Footer>
-  </Modal>
-);
+const ContactModal = ({ show, onHide }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [emailSent, setEmailSent] = useState(false);
+  const [alert, setAlert] = useState(null);
+
+  const isValidEmail = () => {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+  };
+
+  const send = () => {
+    if (name && isValidEmail(email) && message) {
+      // TODO - send mail
+      // See bookmark 'React Contact Form' https://www.webtips.dev/react-contact-form-without-backend
+      setAlert(null);
+      setName('');
+      setEmail('');
+      setMessage('');
+      setEmailSent(true);
+    } else {
+      setAlert(isValidEmail(email) ? 'Please fill in all fields.' : 'Invalid Email');
+    }
+  };
+
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Contact Form
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>Please reach out with any questions or suggestions.</p>
+        <p>Also use this form to report any errors or bugs you have found.</p>
+        <input type="text" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="email" placeholder="Your email address" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <textarea placeholder="Your message" value={message} onChange={(e) => setMessage(e.target.value)} />
+      </Modal.Body>
+      <Modal.Footer>
+        {alert && <span>{alert}</span>}
+        <StyledButton background="#d3d3d3" color="#3e5276" height={38} padding="6px 12px" onClick={onHide}>Cancel</StyledButton>
+        <StyledButton disabled={emailSent} height={38} padding="6px 12px" onClick={send}>{emailSent ? 'Email Sent' : 'Send'}</StyledButton>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
 export default ContactModal;
