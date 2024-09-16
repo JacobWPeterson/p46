926bdffa66e8be788390f0f387282abbbb4e1682
Jacob Peterson <petersonjacobw@gmail.com>
2024-09-17 00:53:06 +0100

    feat: create initial bibliography page

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -7,7 +7,7 @@ import {
 import type { ReactElement } from "react";
 
 import { Workspace } from "../pages/Workspace/index";
-import { Guide } from "../pages/Guide/Guide";
+import { Bibliography } from "../pages/Bibliography/Bibliography";
 import { About } from "../pages/About/About";
 import { E404 } from "../pages/E404/E404";
 
@@ -15,11 +15,11 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route index element={<Workspace />} />
-      <Route path="guide" element={<Guide />} />
+      <Route path="bibliography" element={<Bibliography />} />
       <Route path="about" element={<About />} />
       <Route path="*" element={<E404 />} />
-    </Route>,
-  ),
+    </Route>
+  )
 );
 
 export const Routes = (): ReactElement => <RouterProvider router={router} />;
